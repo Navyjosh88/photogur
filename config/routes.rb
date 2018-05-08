d119caf0d898207e9f659f4b0debda976a043b74
Josh Vargas <navyjosh88@gmail.com>
2018-05-07 23:54:39 -0400

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
+
   devise_for :users
   resources :pictures do
     resources :comments
   resources :users
-  root 'pictures#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
